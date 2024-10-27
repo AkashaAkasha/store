2bce47ee383e67901f0f5e6d44b7054c97c27d27
Valery Sholokhova <akasha2872@gmail.com>
2024-10-27 11:29:22 +0300

    first commit

---
+++ src/main/java/com/example/store/configuration/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.store.configuration;
+
+import com.example.store.services.CustomUserDetailsService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+    private final CustomUserDetailsService userDetailsService;
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+//                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) ->
+                                requests.requestMatchers("/", "/registration")
+                                        .permitAll()
+                        .requestMatchers("/product/**", "/image/**")
+                        .hasAnyAuthority("ROLE_ADMIN","ROLE_USER")
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService)
+//                .passwordEncoder(passwordEncoder());
+//    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
+}
